57a4928b51f010a3e7983f88af585d7556992a7a
VipinKumarMadhaan <vipin.madhaan@gmail.com>
2019-11-16 18:28:12 +0530

    first commit of check weather app

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/b47164534df44320da40f9cfd970caa4/' +
+    latitude +
+    ',' +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' It is currently ' +
+          body.currently.temperature +
+          ' degress out. There is a ' +
+          body.currently.precipProbability +
+          '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
